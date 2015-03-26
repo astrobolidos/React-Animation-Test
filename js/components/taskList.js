c9c87bd02287364ef903d506344eb8993e07512f
astrobolidos <lisandroalavez@gmail.com>
2015-03-26 10:39:34 +0000

    update task list to add control

--- js/components/taskList.js
+++ js/components/taskList.js
@@ -8,17 +8,26 @@ var TaskList = React.createClass({
 	componentWillMount: function(){
 		this.setState({taskList: tasks})
 	},
+	handleKeyPress: function(e) {
+		if( e === undefined ) return;
+		if( e.keyCode != 13 ) return;
+
+		var tasks = this.state.taskList;
+		tasks.push({key:tasks.length, desc: this.refs.newTask.value, subtasks: []});
+		this.setState({taskList: tasks});
+	},
 	render: function(){
 		return (
 			<ol>
-				{ this.state.taskList.map(function(task){
-					return (
-						<li key={task.key}>
-							{task.desc}
-							<SubTaskList subtasks={task.subTasks} />
-						</li>
+				<input type="text" name="newTask" ref="newTask" placeholder="Add a new task!" onKeyDown={this.handleKeyPress()} ></input>
+			{ this.state.taskList.map(function(task){
+				return (
+					<li key={task.key}>
+						{task.desc}
+						<SubTaskList subtasks={task.subTasks} />
+					</li>
 					)
-				})}
+			})}
 			</ol>
 			);
 	}
@@ -27,11 +36,11 @@ var TaskList = React.createClass({
 module.exports = TaskList;
 
 var tasks = [
-	{ key:1, desc:"Have breakfast", subTasks: [ 
-		{ key: 1, desc: 'dring orange juice' },
-		{ key: 2, desc: 'eat porridge' } ] 
-	},
-	{ key:2, desc:"Say goodbye",  subTasks: []	},
-	{ key:3, desc:"Drive to work", subTasks: [] },
-	{ key:4, desc:"Lunch break", subTasks: [] }
+{ key:1, desc:"Have breakfast", subTasks: [ 
+{ key: 1, desc: 'dring orange juice' },
+{ key: 2, desc: 'eat porridge' } ] 
+},
+{ key:2, desc:"Say goodbye",  subTasks: []	},
+{ key:3, desc:"Drive to work", subTasks: [] },
+{ key:4, desc:"Lunch break", subTasks: [] }
 ];
